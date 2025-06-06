e4352306d73abffc25c7d3967bbc5722338007d1
Rafael Augusto Balko de Souza <rafaelbalko@gmail.com>
2025-06-06 13:32:28 -0300

    Adiciona classe MaiorEMenor com métodos para encontrar maior e menor elemento da matriz

--- src/arraysmultidimensionais/MaiorEMenor.java
+++ src/arraysmultidimensionais/MaiorEMenor.java
@@ -1,4 +1,28 @@
 package arraysmultidimensionais;
 
 public class MaiorEMenor {
+
+    public int encontrarMaior(int[][] m, int l, int c) {
+        int ma = m[0][0];
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                if (m[i][j] > ma) {
+                    ma = m[i][j];
+                }
+            }
+        }
+        return ma;
+    }
+
+    public int encontrarMenor(int[][] m, int l, int c) {
+        int me = m[0][0];
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                if (m[i][j] < me) {
+                    me = m[i][j];
+                }
+            }
+        }
+        return me;
+    }
 }
