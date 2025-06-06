91a5be38c058030425e29869c5ca53a34a552459
Rafael Augusto Balko de Souza <rafaelbalko@gmail.com>
2025-06-06 16:02:21 -0300

    Criei a classe principal ProgramaSomaDiagMatriz!

--- src/arraysmultidimensionais/ProgramaSomaDiagMatriz.java
+++ src/arraysmultidimensionais/ProgramaSomaDiagMatriz.java
@@ -1,10 +1,31 @@
 package arraysmultidimensionais;
 
+import java.util.Scanner;
+
 public class ProgramaSomaDiagMatriz {
     public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        SomaDiagMatriz diag = new SomaDiagMatriz();
 
+        System.out.print("Digite o tamanho da Matriz Quadrada: ");
+        int t = sc.nextInt();
 
+        int [][] matriz = new int[t][t];
+
+        System.out.println("Digite os elementos da matriz:");
+        for (int i = 0; i < t; i++){
+            for (int j = 0; j < t; j++){
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        int somaP = diag.somaDP(matriz, t);
+        int somaS = diag.somaDS(matriz, t);
+
+        System.out.println("Resultado da soma da Diagonal Principal: " + somaP);
+        System.out.println("Resultado da soma da Diagonal Secundária: " + somaS);
+
+        sc.close();
     }
-
-
 }
