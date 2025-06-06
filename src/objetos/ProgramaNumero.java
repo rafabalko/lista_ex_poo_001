6f62f746015f59643ff370794757ccc4ecdc4bf4
Rafael Augusto Balko de Souza <rafaelbalko@gmail.com>
2025-06-06 16:27:49 -0300

    Ex 3.1 - Manipulando dados de um objeto feito!

--- src/objetos/ProgramaNumero.java
+++ src/objetos/ProgramaNumero.java
@@ -1,5 +1,18 @@
 package objetos;
 
-public class ProgramaNumero {
+import java.util.Scanner;
 
+public class ProgramaNumero {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        Numero n = new Numero();
+
+        System.out.print("Digite um numero: ");
+        n.valor = sc.nextInt();
+
+        n.mostrarValor();
+        System.out.println("Endereço simulado: " + System.identityHashCode(n));
+
+        sc.close();
+    }
 }
