6175f4c3e012a417153fc9bbf9300fc880264b06
Rafael Augusto Balko de Souza <rafaelbalko@gmail.com>
2025-06-06 12:43:21 -0300

    Ex 1.3 - Fatoração feito!

--- src/metodos/Fatorial.java
+++ src/metodos/Fatorial.java
@@ -1,7 +1,13 @@
 package metodos;
 
-public class Fatorial {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class Fatorial{
+
+    public int calcularFatorial(int n){
+        if (n ==0 || n==1){
+            return 1;
+        } else{
+            return n * calcularFatorial(n - 1);
+        }
     }
 }
+
---
+++ src/metodos/ProgramaFatorial.java
@@ -0,0 +1,22 @@
+package metodos;
+
+import java.util.Scanner;
+
+public class ProgramaFatorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Fatorial f = new Fatorial();
+
+        System.out.print("Digite um numero:");
+        int n = sc.nextInt();
+
+        if (n < 0){
+            System.out.println("Numero não pode ser real, ou negativo!");
+        } else{
+            int r = f.calcularFatorial(n);
+            System.out.println("Fatorial de " + n + "! é: " + r);
+        }
+        sc.close();
+    }
+}
