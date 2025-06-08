1659e42b479f92afacbc44360564057ec2deaabc
Rafael Augusto Balko de Souza <rafaelbalko@gmail.com>
2025-06-08 14:59:13 -0300

    Criei a classe com método ValorContainer

--- src/objetos/ProgramaTroca.java
+++ src/objetos/ProgramaTroca.java
@@ -1,4 +1,9 @@
 package objetos;
 
+import java.util.Scanner;
+
 public class ProgramaTroca {
+    public static void main(String[] args) {
+
+    }
 }
--- src/objetos/ValorContainer.java
+++ src/objetos/ValorContainer.java
@@ -1,4 +1,10 @@
 package objetos;
 
 public class ValorContainer {
+        int valor;
+        public static void trocarValores(ValorContainer refA, ValorContainer refB) {
+            int temp = refA.valor;
+            refA.valor = refB.valor;
+            refB.valor = temp;
+        }
 }
