3e6cb438e41a3a82b038aad91e26dffac92dba9e
Rafael Augusto Balko de Souza <rafaelbalko@gmail.com>
2025-06-08 15:13:58 -0300

    Ex 3.2 Trocando Valores entre Objetos feito!

--- src/objetos/ProgramaTroca.java
+++ src/objetos/ProgramaTroca.java
@@ -4,6 +4,27 @@ import java.util.Scanner;
 
 public class ProgramaTroca {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        ValorContainer objA = new ValorContainer();
+        ValorContainer objB = new ValorContainer();
+
+        System.out.print("Digite o valor de A: ");
+        objA.valor = sc.nextInt();
+
+        System.out.print("Digite o valor de B: ");
+        objB.valor = sc.nextInt();
+
+        System.out.println("\nAntes da troca:");
+        System.out.println("A = " + objA.valor);
+        System.out.println("B = " + objB.valor);
+
+        ValorContainer.trocarValores(objA, objB);
+
+        System.out.println("\nDepois da troca:");
+        System.out.println("A = " + objA.valor);
+        System.out.println("B = " + objB.valor);
+
+        sc.close();
     }
 }
